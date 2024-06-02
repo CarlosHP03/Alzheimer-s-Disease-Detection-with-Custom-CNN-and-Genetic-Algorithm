cb0220d4e64931976db6caf50c94cb0d1a198b64
CarlosHP03 <charlie03hp@gmail.com>
2024-06-02 09:59:19 +0100

    Modified VGG16 scripts

--- VGG16/vgg16_data_augmentation.py
+++ VGG16/vgg16_data_augmentation.py
@@ -11,36 +11,40 @@ ACTIVATION_FUNCTION = "sigmoid"
 AUGMENT = True
 CLASS_WEIGHTS = False
 
-
 # Paths where the train images, test images and validation images are.
 TRAIN_PATH = "../../dataset/train"
 TEST_PATH = "../../dataset/test"
 VAL_PATH = "../../dataset/val"
 
 
-data_handler = ImageDataHandler(TARGET_SIZE, BATCH_SIZE)
+def main():
+    data_handler = ImageDataHandler(TARGET_SIZE, BATCH_SIZE)
 
-# Load training, validation, and test data
-train_generator = data_handler.load_data(TRAIN_PATH, shuffle=AUGMENT)
-valid_generator = data_handler.load_data(VAL_PATH, shuffle=False)
-test_generator = data_handler.load_data(TEST_PATH, test=True, shuffle=False)
+    # Load training, validation, and test data
+    train_generator = data_handler.load_data(TRAIN_PATH, shuffle=AUGMENT)
+    valid_generator = data_handler.load_data(VAL_PATH, shuffle=False)
+    test_generator = data_handler.load_data(TEST_PATH, test=True, shuffle=False)
 
-STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
-STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
-STEP_SIZE_TEST = test_generator.n//test_generator.batch_size
+    step_size_train = train_generator.n // train_generator.batch_size
+    step_size_valid = valid_generator.n // valid_generator.batch_size
+    step_size_test = test_generator.n // test_generator.batch_size
 
-# Create the image classifier object
-model = PreTrainedClassifier(TARGET_SIZE, num_classes=NUM_CLASSES, activation=ACTIVATION_FUNCTION, model_name=MODEL,
-                             class_weight=CLASS_WEIGHTS, augment=AUGMENT)
+    # Create the image classifier object
+    model = PreTrainedClassifier(TARGET_SIZE, num_classes=NUM_CLASSES, activation=ACTIVATION_FUNCTION, model_name=MODEL,
+                                 class_weight=CLASS_WEIGHTS, augment=AUGMENT)
 
-# Train the model
-history = model.train(train_generator, valid_generator, EPOCHS, STEP_SIZE_TRAIN, STEP_SIZE_VALID,)
+    # Train the model
+    history = model.train(train_generator, valid_generator, EPOCHS, step_size_train, step_size_valid, )
 
-model.evaluate(valid_generator, STEP_SIZE_VALID)
+    model.evaluate(valid_generator, step_size_valid)
 
-model.plot_training_performance(history)
+    model.plot_training_performance(history)
 
-test_generator.reset()
-prediction = model.test(test_generator, STEP_SIZE_TEST)
+    test_generator.reset()
+    prediction = model.test(test_generator, step_size_test)
 
-model.evaluation_metrics(prediction, test_generator)
+    model.evaluation_metrics(prediction, test_generator)
+
+
+if __name__ == '__main__':
+    main()
